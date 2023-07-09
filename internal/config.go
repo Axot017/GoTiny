cddbc895e8759e7d6c3534f0bf5b20426d512334
Mateusz Ledwon <mateusz.ledwon@binarycortex.com>
2023-07-09 12:34:35 +0200

    feat: limit max tracking date

--- internal/config.go
+++ internal/config.go
@@ -2,12 +2,14 @@ package internal
 
 import (
 	"os"
+	"strconv"
 )
 
 type Config struct {
-	baseUrl        string
-	logJson        bool
-	linksTableName string
+	baseUrl         string
+	logJson         bool
+	linksTableName  string
+	maxTrackingDays uint
 }
 
 func NewConfig() *Config {
@@ -20,11 +22,17 @@ func NewConfig() *Config {
 	if linksTableName == "" {
 		linksTableName = "links-go-tiny-dev"
 	}
+	maxTrackingDaysStr := os.Getenv("MAX_TRACKING_DAYS")
+	maxTrackingDays, err := strconv.Atoi(maxTrackingDaysStr)
+	if err != nil {
+		maxTrackingDays = 30
+	}
 
 	return &Config{
-		baseUrl:        baseUrl,
-		logJson:        logJson,
-		linksTableName: linksTableName,
+		baseUrl:         baseUrl,
+		logJson:         logJson,
+		linksTableName:  linksTableName,
+		maxTrackingDays: uint(maxTrackingDays),
 	}
 }
 
@@ -38,4 +46,8 @@ func (c *Config) LogJson() bool {
 
 func (c *Config) LinksTableName() string {
 	return c.linksTableName
+}
+
+func (c *Config) MaxTrackingDays() uint {
+	return c.maxTrackingDays
 }
